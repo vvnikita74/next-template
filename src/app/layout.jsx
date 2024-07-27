b4af928e59b76be17cfcd0c13bd9a59440d3f8b1
vvnikita74 <124995385+vvnikita74@users.noreply.github.com>
2024-07-27 15:37:43 +0300

    style upd

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,8 +2,10 @@ import '@/styles/global.css'
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang='en'>
-			<body>{children}</body>
+		<html lang='ru'>
+			<body className='mx-auto my-0 min-w-80'>
+					{children}
+			</body>
 		</html>
 	)
 }
