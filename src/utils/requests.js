4726fc83d6a000fdbeefd1f885afa91b70bf5383
vvnikita74 <124995385+vvnikita74@users.noreply.github.com>
2024-10-29 23:54:28 +0300

    upd

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -14,7 +14,7 @@ export default async function getContent(
 ) {
 	try {
 		const req = await fetch(
-			`${process.env.API_URL}/${path}?${params || ''}`,
+			`${process.env.API_URL}/${process.env.API_PREFIX}/${path}?${params || ''}`,
 			{
 				headers: {
 					Authorization: `Bearer ${process.env.API_KEY}`
